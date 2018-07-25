129958a148ec78cce5eecc71996af8d35e93da86
cgm <caogaoming@qccr.com>
2018-07-25 11:44:09 +0800

    提交

--- src/main/java/org/softnovo/seckill/aop/service/ServiceAspect.java
+++ src/main/java/org/softnovo/seckill/aop/service/ServiceAspect.java
@@ -8,12 +8,10 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.softnovo.seckill.service.SeckillServiceTest;
 import org.springframework.stereotype.Component;
 
 @Component
